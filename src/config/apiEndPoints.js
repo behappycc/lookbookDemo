fde010dcf8f4d06df8ac94ca085d33153c8577c5
Juliachang <facelisten@gmail.com>
2016-12-17 21:50:27 +0800

    chore(build): build script

--- src/config/apiEndPoints.js
+++ src/config/apiEndPoints.js
@@ -1,6 +1,6 @@
 export const API_URL = 'http://140.109.135.165:8000/upload/'
 
-export const PRODUCTION_URL = ''
+export const PRODUCTION_URL = 'http://140.109.135.165:5000/'
 
 export const FRONTEND_URL = 
 	process.env.ENV === 'production' ? PRODUCTION_URL : 'http://localhost:3000/'
