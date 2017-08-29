9ece06df0b5b9e748c10efc1f9d82c6e667dfa62
Juliachang <facelisten@gmail.com>
2017-08-29 13:38:44 +0800

    change ip

--- src/config/apiEndPoints.js
+++ src/config/apiEndPoints.js
@@ -1,6 +1,6 @@
-export const API_URL = process.env.API_URL || 'http://140.109.135.165:8000/upload/'
+export const API_URL = process.env.API_URL || 'http://52.14.156.74:8000/upload/'
 
-export const FRONTEND_PRODUCTION_URL = 'http://hangee.me:8080/'
+export const FRONTEND_PRODUCTION_URL = 'http://52.14.156.74:8080/'
 
 export const FRONTEND_URL = 
 	process.env.ENV === 'production' ? FRONTEND_PRODUCTION_URL : 'http://localhost:3000/'
