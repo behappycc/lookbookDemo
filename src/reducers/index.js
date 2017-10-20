2bed5de6426caf8c1b3f65f0ec0aa0c6e617c741
Hubert <behappyccc@gmail.com>
2017-10-21 01:24:39 +0800

    Add wechat share

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import userReducer from './userReducer'
 import uploadReducer from './uploadReducer'
 import previewImgReducer from './previewImgReducer'
+import urlReducer from './urlReducer'
 
 const rootReducer = combineReducers({
   uploadReducer: uploadReducer,
   userReducer: userReducer,
-  previewImgReducer: previewImgReducer
+  previewImgReducer: previewImgReducer,
+  urlReducer: urlReducer
 })
 
 export default rootReducer
